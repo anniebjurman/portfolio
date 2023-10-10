3a7635a20140db001ad3af50f4f2416c3746663d
Annie Bjurman <id19abn@cs.umu.se>
2023-10-10 15:18:44 +0200

    updates

--- src/components/CreatedWithList.tsx
+++ src/components/CreatedWithList.tsx
@@ -1,6 +1,7 @@
 import "./CreatedWithList.css";
 import { CreatedWith } from "../Enums";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 interface ICreatedWith {
     title: string;
@@ -16,17 +17,28 @@ function CreatedWithList(props: any) {
         props.createdWith.map((cw: CreatedWith) => {
             cwList.push(GetInfo(cw))
         })
-
         setCreatedWith(cwList)
 
-    }, [props.createdWith])
+    }, [props.createdWith, props.accColor])
 
     return (
-        <div>
+        <div className="outerCont">
             {createdWith.map(cw => (
-                <div>
-                    <h1>{cw.title}</h1>
-                </div>
+                <a href={cw.link} target="_blank" style={{ textDecoration: 'none' }}>
+                <motion.div
+                    layout
+                    animate={{ opacity: 1 }}
+                    initial={{ opacity: 0 }}
+                    exit={{ opacity: 0 }}
+                    whileHover={{ scale: 1.1 }}
+                    className="cwCont"
+                >
+                    <div className="iconCont">
+                        <img src={"./images/created_with/" + cw.img} />
+                    </div>
+                    <h2 className="cwTitle">{cw.title}</h2>
+                </motion.div>
+                </a>
             ))}
         </div>
     )
@@ -37,98 +49,98 @@ function GetInfo(index: any): ICreatedWith {
         case CreatedWith.ADOBE_XD:
             return {
                 title: 'Adobe XD',
-                img: '',
-                link: ''
+                img: 'adobe_xd.png',
+                link: 'https://helpx.adobe.com/se/xd/get-started.html'
             }
         case CreatedWith.BLENDER:
             return {
                 title: 'Blender',
-                img: '',
-                link: ''
+                img: 'blender.png',
+                link: 'https://www.blender.org/'
             }
         case CreatedWith.ANDROID_STUDIO:
             return {
                 title: 'Android Studio',
-                img: '',
-                link: ''
+                img: 'android.png',
+                link: 'https://developer.android.com/studio'
             }
         case CreatedWith.CSS:
             return {
                 title: 'CSS',
-                img: '',
-                link: ''
+                img: 'css.png',
+                link: 'https://www.w3schools.com/css/'
             }
         case CreatedWith.FIGMA:
             return {
                 title: 'Figma',
-                img: '',
-                link: ''
+                img: 'figma.png',
+                link: 'https://figma.com'
             }
         case CreatedWith.FIREBASE:
             return {
                 title: 'Firebase',
-                img: '',
-                link: ''
+                img: 'firebase.png',
+                link: 'https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwi4n5CewuuBAxUcCqIDHcR5AtYYABAAGgJsZQ&ase=2&gclid=Cj0KCQjw7JOpBhCfARIsAL3bobdXRAMqdFDBRBUOrEKFoq1_fiwWu7UBb0rtN5njioIR5eh0Y6Onh2gaAkHrEALw_wcB&ohost=www.google.com&cid=CAESVeD2UtCERVYvju3HM61BVtRd_3plKDfVIk0b4kSfE6fglpGVqiLfomIJREC30jtwNFHHQYWiqa8guhUqqzDtHm3-_7ODDIeQ9d7AIY--QOW2-aZrAK4&sig=AOD64_06ybDb-Wd09fLufpnA-NEHCX11lA&q&nis=4&adurl&ved=2ahUKEwiT-IaewuuBAxUcVPEDHbpyAScQ0Qx6BAgGEAE'
             }
         case CreatedWith.FRAMER_MOTION:
             return {
                 title: 'Framer Motion',
-                img: '',
-                link: ''
+                img: 'framer_motion.png',
+                link: 'https://www.framer.com/motion/'
             }
         case CreatedWith.HTML:
             return {
                 title: 'HTML',
-                img: '',
-                link: ''
+                img: 'html.png',
+                link: 'https://www.w3schools.com/html/'
             }
         case CreatedWith.JAVA:
             return {
                 title: 'Java',
-                img: '',
-                link: ''
+                img: 'java.png',
+                link: 'https://www.java.com/sv/'
             }
         case CreatedWith.KOTLIN:
             return {
                 title: 'Kotlin',
-                img: '',
-                link: ''
+                img: 'kotlin.png',
+                link: 'https://kotlinlang.org/'
             }
         case CreatedWith.OPENGL:
             return {
                 title: 'Open GL',
-                img: '',
-                link: ''
+                img: 'open_gl.svg',
+                link: 'https://www.opengl.org/'
             }
         case CreatedWith.PYTHON:
             return {
                 title: 'Python',
-                img: '',
-                link: ''
+                img: 'python.png',
+                link: 'https://www.python.org/'
             }
         case CreatedWith.REACT_NATIVE:
             return {
                 title: 'React Native',
-                img: '',
-                link: ''
+                img: 'react.png',
+                link: 'https://reactnative.dev/'
             }
         case CreatedWith.REACT:
             return {
                 title: 'React',
-                img: '',
-                link: ''
+                img: 'react.png',
+                link: 'https://react.dev/'
             }
         case CreatedWith.TAILWIND:
             return {
                 title: 'Tailwind',
-                img: '',
-                link: ''
+                img: 'tailwind.png',
+                link: 'https://tailwindcss.com/'
             }
         case CreatedWith.TYPESCRIPT:
             return {
                 title: 'Typescript',
-                img: '',
-                link: ''
+                img: 'typescript.png',
+                link: 'https://www.typescriptlang.org/'
             }
         default:
             return {
--- src/pages/ProjectInformation.tsx
+++ src/pages/ProjectInformation.tsx
@@ -2,7 +2,6 @@ import './ProjectInformation.css';
 import { motion } from "framer-motion";
 import { useNavigate, useLocation } from 'react-router-dom';
 import CreatedWithList from '../components/CreatedWithList';
-import { CreatedWith } from '../Enums';
 
 function ProjectInformation(props: any) {
     const navigate = useNavigate();
@@ -23,28 +22,37 @@ function ProjectInformation(props: any) {
         >
             <div className='cardCont' style={{ backgroundColor: project.color }}>
                 <div className='container'>
-                    <div className='icon'>
+                    <div className='icons'>
                         <motion.img
-                            className='icon'
+                            className='i'
                             src={"../icons/back.svg"}
                             alt=""
                             whileHover={{ scale: 1.2 }}
                             transition={{ type: "spring", stiffness: 300, damping: 10 }}
                             onClick={toHome}
                         />
+                        <a href='https://github.com/anniebjurman'
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <motion.img
+                                className='i'
+                                src={"../icons/github.svg"}
+                                alt=""
+                                whileHover={{ scale: 1.2 }}
+                                transition={{ type: "spring", stiffness: 300, damping: 10 }}
+                            />
+                        </a>
                     </div>
-                    <div className='center'>
-                        <h1>{project.title}</h1>
+                    <div className='projTitle'>
+                        <h1 className='projectTitle'>{project.title}</h1>
                     </div>
+                    <p className='desciption'>Enim dolor cillum nisi voluptate et ea est. Qui tempor do fugiat ipsum nulla.
+                        Commodo fugiat ut aliquip exercitation ullamco veniam velit nostrud dolor nostrud proident
+                        consectetur elit eiusmod. Minim deserunt quis aliquip consequat cillum ea magna. Est qui pariatur
+                        laborum labore labore sint. Commodo fugiat ut aliquip exercitation ullamco veniam velit nostrud dolor nostrud proident
+                        consectetur elit eiusmod.</p>
                     <CreatedWithList createdWith={project.createdWith}/>
-                    <p>Enim dolor cillum nisi voluptate et ea est. Qui tempor do fugiat ipsum nulla.
-                        Commodo fugiat ut aliquip exercitation ullamco veniam velit nostrud dolor nostrud proident
-                        consectetur elit eiusmod. Minim deserunt quis aliquip consequat cillum ea magna. Est qui pariatur
-                        laborum labore labore sint.</p>
-                    <p>Enim dolor cillum nisi voluptate et ea est. Qui tempor do fugiat ipsum nulla.
-                        Commodo fugiat ut aliquip exercitation ullamco veniam velit nostrud dolor nostrud proident
-                        consectetur elit eiusmod. Minim deserunt quis aliquip consequat cillum ea magna. Est qui pariatur
-                        laborum labore labore sint.</p>
                 </div>
             </div>
         </motion.div>
